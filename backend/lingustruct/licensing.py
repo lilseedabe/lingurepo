4a64935181bc0e1cf60a98fb817139de98d08b1c
lilseedabe <114091647+lilseedabe@users.noreply.github.com>
2024-11-10 23:27:03 +0900

    Delete backend directory

--- backend/lingustruct/licensing.py
+++
@@ -1,7 +0,0 @@
-class LicenseManager:
-    def __init__(self, license_key):
-        self.license_key = license_key
-
-    def validate(self):
-        # ライセンスキーを検証するロジック
-        return self.license_key == "VALID_LICENSE_KEY"
